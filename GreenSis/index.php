cbbde1d01a7b47086e0e6466766b4dbd4811e954
Rafael Gomes <rafarfl2010@gmail.com>
2021-12-06 15:29:55 -0400

    ajuste na página padrao do wordpress

--- GreenSis/index.php
+++ GreenSis/index.php
@@ -5,22 +5,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<!-- Container do artigo -->	
-	<div class="artigo-container">
-		
-		<!-- Título do post -->
-		<h1>
-			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		</h1>
-		
-		<!-- Categoria -->
-		<?php the_category(); ?>
-		
-		<!-- Autor -->
-		<?php the_author(); ?>
-		
-		<!-- Data -->
-		<?php the_date(); ?>
-		
+	<div class="container">	
 		<!-- Conteúdo do post -->
 		<?php the_content(); ?>
 		
