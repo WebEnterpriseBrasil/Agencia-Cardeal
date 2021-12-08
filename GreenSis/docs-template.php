895ed8b866a862ac183fafa9ec771209824a61b4
erikgs1 <erik.contato15@gmail.com>
2021-12-08 13:00:38 -0300

    adicionando páginas de documentação

---
+++ GreenSis/docs-template.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+Template Name: Documentação
+*/
+
+?>
+
+<?php get_header(); ?>
+<div class="container">
+    <div class="heading">
+     <h2>
+        <?php the_title()?>
+     </h2>
+    </div>
+    <?php the_content() ?>
+</div>
+<?php get_footer(); ?>
