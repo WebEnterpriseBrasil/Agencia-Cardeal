a9a7ae9c88e2b6e668b1954ab9c72c5d75064161
erikgs1 <erik.contato15@gmail.com>
2021-12-11 05:23:07 -0300

    Tema filho funcional

--- greensis-child/functions.php
+++ greensis-child/functions.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' ); 
 
 function enqueue_parent_styles() { 
-  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' ); 
+  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/css/estilo.css' ); 
 } 
 
 // Add Woo Suppport
