d48e47965a1948c8f9b1b0fcb3d46b876c794e73
Rafael Gomes <rafarfl2010@gmail.com>
2021-12-17 22:07:07 -0400

    corrigindo link do instagram no mobile

--- GreenSis/header.php
+++ GreenSis/header.php
@@ -93,7 +93,7 @@
                         <div class="action-bar">
                             <nav class="nav-social">
                                 <a href="https://www.facebook.com/greensis.naturalbeauty" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                                <a href="https://www.instagram.com/greensis.naturalbeauty" target="_blank"></a><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.instagram.com/greensis.naturalbeauty" target="_blank"><i class="fab fa-instagram"></i></a>
                             </nav>
                             <div class="close"><i class="far fa-times-circle"></i></div>         
                         </div>
